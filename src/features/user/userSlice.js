f9e7b4c302c448dffe5af4d5051d515a0a7e95e1
Prashant <prashantpokhrel15@gmail.com>
2022-10-29 22:59:11 +1100

    added user to the local storage and navigate to dashboard

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,10 +1,14 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { toast } from "react-toastify";
 import customFetch from "../../utils/axios";
+import {
+  addUserToLocalStorage,
+  getUserFromLocalStorage,
+} from "../../utils/localStorage";
 
 const initialState = {
   isLoading: false,
-  user: null,
+  user: getUserFromLocalStorage(),
 };
 
 export const registerUser = createAsyncThunk(
@@ -42,6 +46,7 @@ const userSlice = createSlice({
       const { user } = payload;
       state.isLoading = false;
       state.user = user;
+      addUserToLocalStorage(user);
       toast.success(`Hello there ${user.name}`);
     },
     [registerUser.rejected]: (state, { payload }) => {
@@ -54,6 +59,8 @@ const userSlice = createSlice({
     [loginUser.fulfilled]: (state, { payload }) => {
       const { user } = payload;
       state.user = user;
+      addUserToLocalStorage(user);
+
       state.isLoading = false;
       toast.success(`Welcome back ${user.name}`);
     },
